cfd522b05f3ae6527b040e33e9bc080518d63858
maxenced-dev <maxence.desceliers@gmail.com>
2026-05-29 12:51:58 +0200

    gamemanager state machine

---
+++ tiktokshopshop/Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Managers
+{
+    public class GameManager : MonoBehaviour
+    {
+
+        public static GameManager Instance;
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(transform.parent.gameObject);
+                return;
+            }
+
+            Instance = this;
+            DontDestroyOnLoad(transform.parent);
+        }
+        
+        private void Start()
+        {
+            _currentGameState = gameStateAtStart;
+        }
+        
+        #region GameState
+
+        [SerializeField] private GameState gameStateAtStart;
+
+        private enum GameState
+        {
+            Game,
+            Menu
+        }
+
+        private GameState _currentGameState;
+        private GameState CurrentGameState
+        {
+            get => _currentGameState;
+            set
+            {
+                if (_currentGameState == value) 
+                    return;
+                
+                _currentGameState = value;
+                SceneManager.LoadScene(GetSceneByState());
+            }
+        }
+
+        private string GetSceneByState()
+        {
+            return CurrentGameState switch
+            {
+                GameState.Game => "Game",
+                GameState.Menu => "Menu",
+                _ => throw new ArgumentOutOfRangeException()
+            };
+        }
+        #endregion
+        
+    }
+}
